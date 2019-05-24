0b4110bb8e97050c8585f071b1b446a2c1588547
iot-villamtech <sdhthethongthongtinbk@gmail.com>
2019-05-24 18:29:26 +0100

    add more ex for lap2 button

--- Bai2/Lab2/BUTTON_TIMING.py
+++ Bai2/Lab2/BUTTON_TIMING.py
@@ -1,34 +1,29 @@
 import RPi._GPIO as GPIO
 import time
-
-#Khai bao I/O
+#KHai bao I/O
 GPIO.setmode(GPIO.BCM)
-led = 14
 button = 15
-
-GPIO.setup(led, GPIO.OUT)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-#Trang thai Led off
-state_led = False 
-GPIO.output(led, state_led)
 
 try:
     while True:
         GPIO.wait_for_edge(button, GPIO.RISING) #phat hien thanh doi khi 1 canh cua xung thay doi(Nhan nut)
-        print("OT-VILLAMTECH --->Pressed<---")
+        print ("IOT-VILLAMTECH --->Pressed<---")
         start = time.time() #Bat dau tinh thoi gian nhan nut
         time.sleep(0.2)
 
-        while GPIO.input(button) == GPIO.HIGH: #Kiem tra nut da nhan
+        while GPIO.input(button) == GPIO.HIGH:
             time.sleep(0.02)
 
         length = time.time() - start #Thoi gian hien tai tru di thoi gian start
-        print(length) #In thoi gian ra man hinh
-
-        if length > 0.2: #Neu thoi gian lon hown 200ms thi doi trang thai led
-            state_led = ~state_led
-        GPIO.output(led, state_led)  #bat/tat led
+        print (length)
+        #Xuat thoi gian an nut ra man hinh
+        if length > 5:
+            print ("Long Press")
+        elif length > 1:
+            print ("Medium Press")
+        else:
+            print ("Short Press")
 
 except KeyboardInterrupt:
     GPIO.cleanup()
